a7b6d1f5d1b0d658e352835655d8d2b7ef805234
mhussain73 <125877752+mhussain73@users.noreply.github.com>
2025-10-14 22:14:00 +0200

    Update article1.js

--- UTProCricketShop/assets/js/article/article1.js
+++ UTProCricketShop/assets/js/article/article1.js
@@ -1,6 +1,6 @@
 const images = document.querySelectorAll('#main .posts article:nth-of-type(1) .carousel .img-container img');
-const overlay = document.querySelector('#main .overlay');
-const expandedImg = document.getElementById('expanded-img');
+const overlay = document.querySelector('#main .overlay:nth-of-type(1)');
+const expandedImg = document.getElementById('expanded-img1');
 let current = 0;
                 
 function showImage(index) {
