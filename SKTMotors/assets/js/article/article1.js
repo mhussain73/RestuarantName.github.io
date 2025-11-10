cb5a316c5e8ca251fe25f2e44f844716fc8668b2
mhussain73 <125877752+mhussain73@users.noreply.github.com>
2025-11-10 22:33:01 +0200

    Update article1.js

--- SKTMotors/assets/js/article/article1.js
+++ SKTMotors/assets/js/article/article1.js
@@ -1,4 +1,5 @@
-const images = document.querySelectorAll('#main .posts.active article:nth-of-type(1) .carousel .img-container img');
+const activeSection = document.querySelector('#main .posts.active');
+const images = activeSection.querySelectorAll('article:nth-of-type(1) .carousel .img-container img');
 const overlay = document.querySelector('#main .overlay:nth-of-type(1)');
 const expandedImg = document.getElementById('expanded-img1');
 let current = 0;
@@ -24,12 +25,12 @@ function updateOverlayImage(j) {
 let startX = 0; // To store where the swipe starts
 				
 // Detect when user touches the screen
-document.querySelector('#main .posts.active article:nth-of-type(1) .carousel').addEventListener('touchstart', (e) => {
+activeSection.querySelector('article:nth-of-type(1) .carousel').addEventListener('touchstart', (e) => {
   	startX = e.touches[0].clientX;
 });
 				
 // Detect when user lifts their finger
-document.querySelector('#main .posts.active article:nth-of-type(1) .carousel').addEventListener('touchend', (e) => {
+activeSection.querySelector('article:nth-of-type(1) .carousel').addEventListener('touchend', (e) => {
   	const endX = e.changedTouches[0].clientX;
   	const diffX = endX - startX;
 					
